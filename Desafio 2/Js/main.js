4d2b2e63658cf04b83e3786947e9e96f38c0b229
Ignacio Martinez <ignaciomartinez923@gmail.com>
2021-11-04 12:28:32 -0300

    agregando desafio 2

---
+++ Desafio 2/Js/main.js
@@ -0,0 +1,9 @@
+let nombre = prompt ("Ingrese su nombre");
+let numeroIngresado = prompt ("Ingrese un numero entre 10 y 50")
+
+if ((numeroIngresado >= 10) && (numeroIngresado <=50)){
+    alert (nombre + " Felicidades, usted acertó el numero correcto ");
+} else {
+    alert ("Error, por favor ingrese otro número");
+    
+}
